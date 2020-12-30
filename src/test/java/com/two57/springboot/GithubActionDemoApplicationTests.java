af16f6dc8d4e5302f456e0ed43de07c27f4d5dbf
Amit Patel <amit.patel1@spglobal.com>
2020-12-29 23:27:17 -0500

    first commit

---
+++ src/test/java/com/two57/springboot/GithubActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.two57.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
